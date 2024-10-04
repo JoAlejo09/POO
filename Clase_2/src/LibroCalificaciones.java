4aed8faeb2da8ecd24c4756014bf40b4bde9260b
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-04 12:03:05 -0500

    Modificacion Clases con parametros

--- Clase_2/src/LibroCalificaciones.java
+++ Clase_2/src/LibroCalificaciones.java
@@ -1,4 +1,11 @@
 public class LibroCalificaciones {
+    private String nombreCurso;
+    public void getNombreCurso(String nombre){
+        nombreCurso = nombre;
+    }
+    public String setNombreCurso(){
+        return nombreCurso;
+    }
     public void mostrarMensaje(String curso){
         System.out.println("Bienvenidos al Libro de Calificaciones del curso: "+curso);
     }
