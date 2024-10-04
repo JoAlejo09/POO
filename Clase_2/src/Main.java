8b2d56a4ce8d392c9e75e94c05c9ec3f3ad64008
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-04 11:41:05 -0500

    Edicion 1 Clase 2

--- Clase_2/src/Main.java
+++ Clase_2/src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+public class Main { //Definicion de las clases para Java
+    //Establecimiento de clases publicas y con el nombre de la clase
     public static void main(String[] args) {
+        //Establecimiento del metodo solo existe un metodo main para cada programa
+
         System.out.println("Hello world!");
     }
 }
