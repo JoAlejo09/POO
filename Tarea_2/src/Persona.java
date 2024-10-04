883ca6b930efad28fba7371e66ef1ec5df9900a1
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-04 12:19:01 -0500

    Add Class Persona and create a new method

---
+++ Tarea_2/src/Persona.java
@@ -0,0 +1,8 @@
+public class Persona {
+    public String nombre;
+    public int edad;
+    public String genero;
+    public void Presentarse(){
+        System.out.println("Mi nombre es: "+nombre+"\nTengo "+edad+" años\nMi genero es "+genero);
+    }
+}
