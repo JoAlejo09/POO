f49c415f50778eb2b2055d8879fa64b93191c9ad
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-04 12:58:35 -0500

    Commit 3

--- Taller_Github/src/Main.java
+++ Taller_Github/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Mi nombre es Jose Pila");
         System.out.println("Mi direccion es Monjas - Quito Ecuador");
-
+        System.out.println("Mi numero de telefono es 0963289100");
     }
 }
