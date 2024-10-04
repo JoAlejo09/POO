78b21b9791be252f080059afdf091f3d58d2f16e
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-04 12:32:28 -0500

    Add getters and setters create object in main function

--- Tarea_2/src/Main.java
+++ Tarea_2/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona nueva_Persona = new Persona();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre y apellido");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese su edad");
+        int edad = scanner.nextInt();
+        System.out.println("Ingrese su genero 1.M / 2.F ");
+        int genero = scanner.nextInt();
+        if (genero==1)
+            nueva_Persona.setGenero("Masculino");
+        else
+            nueva_Persona.setGenero("Femenino");
+        nueva_Persona.setNombre(nombre);
+        nueva_Persona.setEdad(edad);
+        nueva_Persona.Presentarse();
+
     }
+
 }
--- Tarea_2/src/Persona.java
+++ Tarea_2/src/Persona.java
@@ -2,6 +2,31 @@ public class Persona {
     public String nombre;
     public int edad;
     public String genero;
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public void Presentarse(){
         System.out.println("Mi nombre es: "+nombre+"\nTengo "+edad+" años\nMi genero es "+genero);
     }
