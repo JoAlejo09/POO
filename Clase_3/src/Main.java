96748e36ebad4e5cb3c9ec3c5b4b011d39069858
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-10 07:32:53 -0500

    Creation Clase_3

---
+++ Clase_3/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String asignatura,dias;
+        int horas;
+        System.out.print("Ingrese el nomrbre de la asignatura: ");
+        asignatura = scanner.nextLine();
+        System.out.print("Ingrese los dias de clases: ");
+        dias = scanner.nextLine();
+        System.out.print("Ingrese las horas de clase: ");
+        horas = scanner.nextInt();
+        System.out.println("-----------------------------------------------------------------");
+        System.out.print("Nombre asignatura: "+asignatura+"\n Dias de clase: "+dias+"\nHoras de clase a la semana: "+horas);
+
+    }
+}
