5f607685af9585d2b6f80836d0e045f64f4b112f
josea <josealejandro_pilavizuete@hotmail.com>
2024-10-16 09:11:35 -0500

    Actualizacion Ejercicio 2

--- Tareas/Tarea_S2/Practica 2/src/Main.java
+++ Tareas/Tarea_S2/Practica 2/src/Main.java
@@ -1,7 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double salario;
+        String nombre, cargo, ingreso;
 
+        //Ingreso 1
+        System.out.print("Ingrese el nombre del empleado: ");
+        nombre = scanner.nextLine();
+        System.out.print("\nIngrese la fecha en la que ingreso: ");
+        ingreso = scanner.nextLine();
+        System.out.print("\nIngrese el cargo que ocupa: ");
+        cargo = scanner.nextLine();
+        System.out.print("\nIngrese el salario que gana el empleado: ");
+        salario = scanner.nextDouble();
+        Empleado empleado_1 = new Empleado(nombre, cargo, ingreso, salario);
+        empleado_1.mostrarEmpleado();
+        scanner.nextLine();
+
+        //Ingreso 2
+        System.out.print("Ingrese el nombre del empleado: ");
+        nombre = scanner.nextLine();
+        System.out.print("\nIngrese la fecha en la que ingreso: ");
+        ingreso = scanner.nextLine();
+        System.out.print("\nIngrese el cargo que ocupa: ");
+        cargo = scanner.nextLine();
+        System.out.print("\nIngrese el salario que gana el empleado: ");
+        salario = scanner.nextDouble();
+        Empleado empleado_2 = new Empleado(nombre, cargo, ingreso, salario);
+        empleado_2.mostrarEmpleado();
+        scanner.nextLine();
+
+        //Ingreso 3
+        System.out.print("Ingrese el nombre del empleado: ");
+        nombre = scanner.nextLine();
+        System.out.print("\nIngrese la fecha en la que ingreso: ");
+        ingreso = scanner.nextLine();
+        System.out.print("\nIngrese el cargo que ocupa: ");
+        cargo = scanner.nextLine();
+        System.out.print("\nIngrese el salario que gana el empleado: ");
+        salario = scanner.nextDouble();
+        Empleado empleado_3 = new Empleado(nombre, cargo, ingreso, salario);
+        empleado_3.mostrarEmpleado();
+        scanner.nextLine();
     }
 }
--- Tareas/Tarea_S2/Práctica 1/src/Main.java
+++ Tareas/Tarea_S2/Práctica 1/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         String nombre, posicion;
         int edad, camiseta;
         Scanner scanner = new Scanner(System.in);
+        //Ingreso 1
         System.out.print("Ingrese el nombre del jugador: ");
         nombre = scanner.nextLine();
         System.out.print("\nIngrese la posicion donde juega el jugador: ");
@@ -15,10 +16,11 @@ public class Main {
         edad = scanner.nextInt();
         System.out.print("\nIngrese el numero de la camiseta del jugador: ");
         camiseta = scanner.nextInt();
+        //Objeto 1
         Jugador jugador_1 = new Jugador(nombre, posicion, edad, camiseta);
         jugador_1.mostrarJugador();
         scanner.nextLine();
-
+        //Ingreso 2
         System.out.print("Ingrese el nombre del jugador: ");
         nombre = scanner.nextLine();
         System.out.print("\nIngrese la posicion donde juega el jugador: ");
@@ -27,11 +29,11 @@ public class Main {
         edad = scanner.nextInt();
         System.out.print("\nIngrese el numero de la camiseta del jugador: ");
         camiseta = scanner.nextInt();
+        //Objeto 2
         Jugador jugador_2 = new Jugador(nombre, posicion, edad, camiseta);
         jugador_2.mostrarJugador();
         scanner.nextLine();
-
-
+        //Ingreso 3
         System.out.print("Ingrese el nombre del jugador: ");
         nombre = scanner.nextLine();
         System.out.print("\nIngrese la posicion donde juega el jugador: ");
@@ -40,6 +42,7 @@ public class Main {
         edad = scanner.nextInt();
         System.out.print("\nIngrese el numero de la camiseta del jugador: ");
         camiseta = scanner.nextInt();
+        //Objeto 3
         Jugador jugador_3 = new Jugador(nombre, posicion, edad, camiseta);
         jugador_3.mostrarJugador();
         scanner.nextLine();
