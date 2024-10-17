727db87013fde195a04de107208c405cfb93309b
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-17 09:06:18 -0500

    Taller 3

--- Talleres/Taller_3/src/Jugador.java
+++ Talleres/Taller_3/src/Jugador.java
@@ -1,2 +1,21 @@
 public class Jugador {
+    public String avatar, pais;
+    public int edad;
+
+    public Jugador(int edad, String pais, String avatar) {
+        this.edad = edad;
+        this.pais = pais;
+        this.avatar = avatar;
+    }
+    public Jugador(){
+    }
+    public void patear(){
+        System.out.println("El jugador " + avatar + "Esta pateando....");
+    }
+    public void correr(){
+        System.out.println("El jugador " + avatar + "Esta corriendo....");
+    }
+    public void saltar(){
+        System.out.println("El jugador " + avatar + "Esta saltando....");
+    }
 }
--- Talleres/Taller_3/src/Main.java
+++ Talleres/Taller_3/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Registro de informacion de Jugadores");
+        Jugador jugador1 = new Jugador(18,"Ecuador","Andy");
+        Jugador jugador2 = new Jugador();
+        jugador1.saltar();
     }
 }
