bed16b4e51db9b386fc397224a78a3f7530005d5
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-24 07:31:25 -0500

    Clase_5 Update

--- Clases/Clase_5/src/Octubre24/Main.java
+++ Clases/Clase_5/src/Octubre24/Main.java
@@ -1,4 +1,8 @@
 package Octubre24;
 
 public class Main {
+    public static void main(String[] args) {
+        Operaciones op = new Operaciones(2,5);
+
+    }
 }
---
+++ Clases/Clase_5/src/Octubre24/Operaciones.java
@@ -0,0 +1,17 @@
+package Octubre24;
+
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
+public class Operaciones {
+    //Atributos
+    int valor1,valor2;
+    //Constructor
+    public Operaciones(int val1, int val2){
+        this.valor1 = val1;
+        this.valor2 = val2;
+    }
+    //Metodos
+    public void imprimir(){
+
+    }
+}
