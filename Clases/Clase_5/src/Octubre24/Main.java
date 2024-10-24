e8d0a42ad83022d242ea9483100564086de2d286
POO <josealejandro_pilavizuete@hotmail.com>
2024-10-24 07:38:20 -0500

    Clase_5 Update

--- Clases/Clase_5/src/Octubre24/Main.java
+++ Clases/Clase_5/src/Octubre24/Main.java
@@ -2,7 +2,10 @@ package Octubre24;
 
 public class Main {
     public static void main(String[] args) {
-        Operaciones op = new Operaciones(2,5);
-
+        Operaciones op = new Operaciones(33,25);
+        op.imprimir();
+        op.suma();
+        op.resta();
+        op.producto();
     }
 }
--- Clases/Clase_5/src/Octubre24/Operaciones.java
+++ Clases/Clase_5/src/Octubre24/Operaciones.java
@@ -12,6 +12,15 @@ public class Operaciones {
     }
     //Metodos
     public void imprimir(){
-
+        System.out.println("Los numeros con los que se van a realizar las operaciones son: "+valor1+" y "+valor2);
+    }
+    public void suma(){
+        System.out.println("La suma de "+valor1+" + "+valor2+" = "+(valor1+valor2));
+    }
+    protected void resta(){
+        System.out.println("La resta de "+valor1+" - "+valor2+" = "+(valor1-valor2));
+    }
+    void producto(){
+        System.out.println("El producto de "+valor1+" * "+valor2+" = "+(valor1*valor2));
     }
 }
