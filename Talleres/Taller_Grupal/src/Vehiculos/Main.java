19b32cd4ae3c04f71204e3b60d1bb23940be8f96
josea <josealejandro_pilavizuete@hotmail.com>
2024-11-22 08:58:58 -0500

    UPDATE TALLER GRUPAL EXPOSICION

--- Talleres/Taller_Grupal/src/Vehiculos/Main.java
+++ Talleres/Taller_Grupal/src/Vehiculos/Main.java
@@ -4,17 +4,23 @@ public class Main {
     public static void main(String []args){
 
         //CREACION DE LOS OBJETOS E INSTANCIA DE CLASES
-        Vehiculo vehiculo1 = new Automovil("Toyota", "Corolla", 2018, 20000, 50000, 4, "Gasolina");
-        Vehiculo vehiculo2 = new Automovil("Tesla", "Model 3", 2022, 45000, 20000, 4, "Eléctrico");
-        Vehiculo vehiculo3 = new Motocicleta("Yamaha", "R3", 2021, 8000, 15000, 321, "Gasolina");
-        Vehiculo vehiculo4 = new Motocicleta("Zero", "SR/F", 2023, 18000, 5000, 0, "Eléctrico");
+        Automovil auto1 = new Automovil("Toyota", "Corolla", 2018, 20000, 50000, 4, "Gasolina");
+        Automovil auto2 = new Automovil("Tesla", "Model 3", 2022, 45000, 20000, 4, "Eléctrico");
+        Motocicleta moto1 = new Motocicleta("Yamaha", "R3", 2021, 8000, 15000, 321, "Gasolina");
+        Motocicleta moto2 = new Motocicleta("Zero", "SR/F", 2023, 18000, 5000, 0, "Eléctrico");
 
         // Mostrar información y cálculos
-        Vehiculo[] vehiculos = {vehiculo1, vehiculo2, vehiculo3, vehiculo4};//CREACION DE ARREGLO DE OBJETOS
-        for (Vehiculo vehiculo : vehiculos) {
             System.out.println("=================================");
-            System.out.println(vehiculo.mostrarInformacion());
-            System.out.println("Valor depreciado: $" + vehiculo.calcularDepreciacion());
+            System.out.println(auto1.mostrarInformacion());
+            System.out.println("Valor depreciado: $" + auto1.calcularDepreciacion());
+            System.out.println("=================================");
+            System.out.println(auto2.mostrarInformacion());
+            System.out.println("Valor depreciado: $" + auto2.calcularDepreciacion());
+            System.out.println("=================================");
+            System.out.println(moto1.mostrarInformacion());
+            System.out.println("Valor depreciado: $" + moto1.calcularDepreciacion());
+            System.out.println("=================================");
+            System.out.println(moto2.mostrarInformacion());
+            System.out.println("Valor depreciado: $" + moto2.calcularDepreciacion());
         }
     }
-}
