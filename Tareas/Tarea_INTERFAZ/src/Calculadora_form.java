e4e3d3960518df593ec467ba888e47054a7d916a
josea <josealejandro_pilavizuete@hotmail.com>
2024-12-10 14:25:06 -0500

    COMMIT - TAREA INTERFACES

--- Tareas/Tarea_INTERFAZ/src/Calculadora_form.java
+++ Tareas/Tarea_INTERFAZ/src/Calculadora_form.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Calculadora_form {
+    public JPanel jPanel_C;
+    private JTextField tNum1;
+    private JTextField textField2;
+    private JButton sumarButton;
+    private JButton restarButton;
+    private JButton multiplicarButton;
+    private JButton dividirButton;
+    private JTextField textField3;
+    private JLabel tNum2;
 }
--- Tareas/Tarea_INTERFAZ/src/Login_form.java
+++ Tareas/Tarea_INTERFAZ/src/Login_form.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,8 +14,30 @@ public class Login_form {
         bAcceder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                boolean valor = validar();
+                if(valor){
+                    jPanel.setVisible(false);
+                    new Login_form().jPanel.setVisible(false);
+                    JFrame frame = new JFrame("CALCULAORA");
+                    frame.setContentPane(new Calculadora_form().jPanel_C);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setSize(40,60);
+                    frame.setPreferredSize(new Dimension(640,480));
+                    frame.pack();
+                    frame.setVisible(true);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Login incorrecto. Ingreselo nuevamente...","Error",JOptionPane.ERROR_MESSAGE);
+                    tUsuario.setText("");
+                    pContraseña.setText("");
+                }
 
             }
         });
+    }
+    public boolean validar(){
+        int valor = tUsuario.getText().compareToIgnoreCase("Pila");
+        int valor_1 = pContraseña.getText().compareToIgnoreCase("Pila123");
+        if(valor ==0 && valor_1==0)
+        {return true;}else{ return false;}
     }
 }
