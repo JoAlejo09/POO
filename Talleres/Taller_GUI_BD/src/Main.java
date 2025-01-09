882b9563602490542351f2f7e31e864298580b54
POO <josealejandro_pilavizuete@hotmail.com>
2025-01-09 08:14:59 -0500

    CREATE TALLER_BD_GUI

--- Talleres/Taller_GUI_BD/src/Main.java
+++ Talleres/Taller_GUI_BD/src/Main.java
@@ -1,4 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/concursos";
+        String usuario = "root";
+        String password = "1234";
+        try{
+            Connection conn = DriverManager.getConnection(url,usuario,password);
+            if (conn != null) {
+                System.out.println("Conexión establecida a la base de datos.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
